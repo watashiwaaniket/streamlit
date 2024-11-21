9f166a9fc89a72fc289bb2b08378a3682e190476
Aniket Dhakane <aniketdhakane3@gmail.com>
2024-11-21 20:19:05 +0530

    dna sequence count webapp

--- simple-stock-price/myapp.py
+++ simple-stock-price/myapp.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.write("""
 # Simple  stock Price App
          
-Shown are the stock closing price and volume of Google!
+Shown are the stock **closing price** and volume of ***Nvidia***!
 
 """)
 
@@ -14,8 +14,14 @@ tickerSymbol = 'NVDA'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2018-5-31', end='2024-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2024-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
